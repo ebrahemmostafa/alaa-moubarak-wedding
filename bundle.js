0c7736b05b7ee334123097cc6ae8e2180daf9437
PickyPets <ebrahemmostafa19999@gmail.com>
2026-08-01 12:13:55 +0300

    Move hero section text up by 3 lines (paddingTop: 40px)

--- bundle.js
+++ bundle.js
@@ -21801,7 +21801,7 @@ function mF({ name1: e, name2: t, date: n, subtitle: r, paused: s }) {
       }),
       p.jsxs("div", {
         className: "relative z-10 flex flex-col items-center text-center px-6",
-        style: { paddingTop: "115px" },
+        style: { paddingTop: "40px" },
         children: [
           p.jsx(X.p, {
             initial: { opacity: 0, y: 20 },
